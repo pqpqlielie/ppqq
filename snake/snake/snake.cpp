607137e3196a680d6c22ca87d7e7a4695148fe08
Your Name <you@example.com>
2019-10-29 16:58:07 +0800

    update

--- snake/snake/snake.cpp
+++ snake/snake/snake.cpp
@@ -2,10 +2,31 @@
 //
 
 #include "stdafx.h"
-
-
-int main()
+#include<Windows.h>
+void Init()
 {
+
+}
+
+void GameLoop()
+{
+	while (1)
+	{
+		Sleep(100);
+}
+}
+
+void Score()
+{
+
+}
+
+
+int main(int argc, char* argv[])
+{
+	Init();
+	GameLoop();
+	Score();
     return 0;
 }
 
